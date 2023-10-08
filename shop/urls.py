33b887120345c49868e853ad203f79ba8a2eb0d0
Art01ur <bakuntsartur93@gmail.com>
2023-10-08 15:49:12 +0400

    add view and HTML for product buying

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:product_id>', views.product_details, name='product_details')
+    path('product/<int:product_id>', views.product_details, name='product_details'),
+    path('buy/<int:product_id>', views.product_buying, name='product_buying')
 ]
--- shop/views.py
+++ shop/views.py
@@ -13,3 +13,13 @@ def product_details(request, product_id):
     return render(request, 'product_details.html', {'product': product})
 
 
+def product_buying(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if product.quantity > 0:
+        product.quantity -= 1
+        product.save()
+        message = 'Your order has been successfully completed'
+    else:
+        message = "Sorry, we don't have this product in stock"
+
+    return render(request, 'product_buying.html', {'message': message})
